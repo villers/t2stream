b9fed6d9aa6122db0e61fbb04fbefe22cdcedb79
VILLERS Mickaël <mickael.villers@epitech.eu>
2015-11-10 00:36:27 +0100

    ajout du module autentication

---
+++ src/client/app/modules/authentication/index/index.ctrl.ts
@@ -0,0 +1,16 @@
+/// <reference path="../../../../client.d.ts" />
+
+module t2stream.modules.authentication {
+    'use strict';
+
+    export class IndexCtrl {
+        /** @ngInject */
+        constructor() {
+
+        }
+
+        doClick() {
+            alert('You are crazy!!!');
+        }
+    }
+}
