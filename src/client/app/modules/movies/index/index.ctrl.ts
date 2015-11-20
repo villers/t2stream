d52ad448943428891d9de1191aa3f88b4c1f65be
Thomas ROCHA <thomas.rocha@epitech.eu>
2015-11-20 16:50:05 +0100

    Movies module - Stars title + seen/not seen added

--- src/client/app/modules/movies/index/index.ctrl.ts
+++ src/client/app/modules/movies/index/index.ctrl.ts
@@ -23,17 +23,22 @@ module t2stream.modules.movies {
         public title: string;
         public figure: FigureFoundation;
         public description: FilmDescriptionFoundation;
+        public rating: number;
+        public seen: boolean;
     }
 
     export class IndexController {
 
         public films: Array<FilmFoundation> = [];
+        public repeatInt:any;
         /** @ngInject */
         constructor() {
+
+            this.repeatInt = this.makeArray;
             var tpl = {
-                title: 'Film #',
+                title: 'Exemple de film #',
                 figure: {
-                    img: 'http://www.avoir-alire.com/IMG/arton28831.jpg?1416860220'
+                    img: '../assets/images/films/film.jpg'
                 },
                 description: {
                     snippet: 'Lorem du Film #',
@@ -45,9 +50,25 @@ module t2stream.modules.movies {
                 f.title = tpl.title + i;
                 f.figure = {img: tpl.figure.img};
                 f.description = {snippet: tpl.description.snippet + i, full:  '#' + i + tpl.description.full};
+                f.rating = this.randomNote(0, 5);
+                f.seen = this.randomNote(0, 1);
                 this.films.push(f);
             }
         }
+
+        randomNote(min: number = 0, max: number = 5) {
+            var r = Math.round(Math.round(Math.random() * 10 / 2) + Math.round(Math.random() * 10 / .25) * .75);
+            return r > max || r < min ? this.randomNote(min, max) : r;
+        }
+
+        private makeArray(u) {
+            var arr = [];
+            for (var i = 0; i < u; i++) {
+                arr.push(i);
+            }
+            return arr;
+        }
+
 
         doClick() {
             alert('You are crazy!!!');
