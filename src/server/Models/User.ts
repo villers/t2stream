3ffa69f9b587d556e050cdb4e6f62c9c147a097a
Fabrice didierjean <fabrice.didierjean@gmail.com>
2015-12-04 04:25:38 +0100

    Add Model Creation DTS only remove import

---
+++ src/server/Models/User.ts
@@ -0,0 +1,8 @@
+/// <reference path="../TypeFramework.d.ts" />
+
+import { Model } from '../Model';
+
+export class User extends Model {
+	Username: string;
+	Password: string;
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -3,8 +3,12 @@
 import { Application } from './Application';
 var app : Application = new Application(__dirname);
 
+import { User } from './Models/User';
+
 app.configure(() => {
 	app.config.addJson('config/app.json');
+	app.addDeclaration('Models.d.ts');
+	app.addModel(User);
 });
 
 app.start();
