91adb845d60a12eb74712380b91b87a4616c6765
VILLERS Mickaël <mickael.villers@epitech.eu>
2015-12-14 23:42:12 +0100

    fix nodemon + correct code style

--- src/server/helpers/authentication.ts
+++ src/server/helpers/authentication.ts
@@ -1,56 +1,56 @@
 /// <reference path="../index.d.ts" />
 
 import {Request} from 'express';
-import * as bcrypt from 'bcrypt';
-import * as jwt from 'jsonwebtoken';
+import {compare} from 'bcrypt';
+import {verify, sign} from 'jsonwebtoken';
 import {config} from '../config/config';
 import {User} from '../models/user';
 
-import {GetApplication} from '../index';
+import {GetApplication, } from '../index';
 
 export function getAccessToken(bearerToken: string, callback: (error: boolean | any, accessToken: any) => void) {
-	jwt.verify(bearerToken, config.token.secret, {issuer: config.token.issuer}, (err: Error, decoded: any) => {
-		if (err) {
-			callback(false, null);
-		}
-		if (decoded.Type !== 'accessToken') {
-			callback(false, null);
-		}
-		callback(false, decoded);
-	});
-};
+    verify(bearerToken, config.token.secret, {issuer: config.token.issuer}, (err: Error, decoded: any) => {
+        if (err) {
+            callback(false, null);
+        }
+        if (decoded.Type !== 'accessToken') {
+            callback(false, null);
+        }
+        callback(false, decoded);
+    });
+}
 
 export function getClient(clientId: string, clientSecret: string, callback: (error: boolean, client: any) => void) {
-	callback(false, {clientId : 't2stream'});
-};
+    callback(false, {clientId : 't2stream'});
+}
 
 export function grantTypeAllowed(clientId: string, grantType: string, callback: (error: boolean, allowed: boolean) => void) {
-	callback(false, true);
-};
+    callback(false, true);
+}
 
 export function getUser(username: string, password: string, callback: (error: boolean | any, user: any) => void) {
-	let ModelUsers = GetApplication().models.user;
-	ModelUsers.findOne({username: username}).exec((err, model) => {
-		if (err) {
-			callback(false, null);
-		}
-		bcrypt.compare(password, model.password, (err: Error, same: boolean) => {
-			if (same) {
-				callback(false, model);
-			} else {
-				callback(false, null);
-			}
-		});
-	});
-};
+    let ModelUsers = GetApplication().models.user;
+    ModelUsers.findOne({username: username}).exec((err, model) => {
+        if (err) {
+            callback(false, null);
+        }
+        compare(password, model.password, (err: Error, same: boolean) => {
+            if (same) {
+                callback(false, model);
+            } else {
+                callback(false, null);
+            }
+        });
+    });
+}
 
 export function generateToken(type: string, req: Request, callback: (error: boolean, object: any) => void) {
 	let User = req.user;
-	User.Type = type;
-	let Token = jwt.sign(User, config.token.secret, { expiresIn: config.token.expiresInSeconds, issuer: config.token.issuer });
-	if (type === 'accessToken') {
-		callback(false, { accessToken : Token });
-	} else {
-		callback(false, { refreshToken  : Token });
-	}
-};
+    User.Type = type;
+    let Token = sign(User, config.token.secret, { expiresIn: config.token.expiresInSeconds, issuer: config.token.issuer });
+    if (type === 'accessToken') {
+        callback(false, { accessToken : Token });
+    } else {
+        callback(false, { refreshToken  : Token });
+    }
+}
